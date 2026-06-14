46fa18b56e26da42d3940efcb4ca97f9d94dc990
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-14 20:03:16 -0300

    Update ChatModel.dart

--- lib/Model/ChatModel.dart
+++ lib/Model/ChatModel.dart
@@ -1,20 +1,40 @@
 class ChatModel {
-  String name;
-  String icon;
-  bool isGroup;
-  String time;
-  String currentMessage;
-  String status;
-  bool select = false;
-  int id;
+  final String id;
+  final String name;
+  final String? avatar;
+  final bool isGroup;
+  final String lastMessage;
+  final String time;
+  final int unreadCount;
+  final bool isOnline;
+  final String? phone;
+  final String? email;
+
   ChatModel({
-    this.name,
-    this.icon,
-    this.isGroup,
-    this.time,
-    this.currentMessage,
-    this.status,
-    this.select = false,
-    this.id,
+    required this.id,
+    required this.name,
+    this.avatar,
+    this.isGroup = false,
+    this.lastMessage = '',
+    this.time = '',
+    this.unreadCount = 0,
+    this.isOnline = false,
+    this.phone,
+    this.email,
   });
+
+  factory ChatModel.fromMap(Map<String, dynamic> map) {
+    return ChatModel(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      avatar: map['avatar_url'],
+      isGroup: map['is_group'] ?? false,
+      lastMessage: map['last_message'] ?? '',
+      time: map['last_message_time'] ?? '',
+      unreadCount: map['unread_count'] ?? 0,
+      isOnline: map['is_online'] ?? false,
+      phone: map['phone'],
+      email: map['email'],
+    );
+  }
 }
